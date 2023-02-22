ca6d2d560cb477fc4ed649cafb0534be8f9ab607
Kyle McCormick <kdmc@pm.me>
2023-02-22 11:25:39 -0500

    build: add shellcheck workflow for PRs and master
    Additionally, this brings all existing shell scripts
    into compliance with ShellCheck.
    
    This implements part of an ADR, proposed here:
    https://github.com/openedx/.github/pull/60

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -13,7 +13,7 @@ set -e
 # Violations thresholds for failing the build
 source scripts/thresholds.sh
 
-XSSLINT_THRESHOLDS=`cat scripts/xsslint_thresholds.json`
+XSSLINT_THRESHOLDS=$(cat scripts/xsslint_thresholds.json)
 export XSSLINT_THRESHOLDS=${XSSLINT_THRESHOLDS//[[:space:]]/}
 
 
